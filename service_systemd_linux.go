f2b341c23d95ad863e19b3310c95184faf15a366
Anthony "Ishpeck" Tedjamulia <ishpeck@debbox02>
2020-06-03 13:35:48 -0600

    Fixing systemd complaint that service configurations are world-inaccessible after install

--- service_systemd_linux.go
+++ service_systemd_linux.go
@@ -132,7 +132,7 @@ func (s *systemd) Install() error {
 		return fmt.Errorf("Init already exists: %s", confPath)
 	}
 
-	f, err := os.Create(confPath)
+	f, err := os.OpenFile(confPath, os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		return err
 	}
