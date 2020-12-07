cd7304cd2c8b65d67cbf0d92032fdbb6f31b860d
Karen Almog <kalmog@mirantis.com>
2020-12-07 17:10:22 +0100

    Move import paths to k0sproject

--- service.go
+++ service.go
@@ -18,7 +18,7 @@
 //	import (
 //		"log"
 //
-//		"github.com/kardianos/service"
+//		"github.com/k0sproject/service"
 //	)
 //
 //	var logger service.Logger
@@ -59,7 +59,7 @@
 //			logger.Error(err)
 //		}
 //	}
-package service // import "github.com/kardianos/service"
+package service // import "github.com/k0sproject/service"
 
 import (
 	"errors"
--- service_test.go
+++ service_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/kardianos/service"
+	"github.com/k0sproject/service"
 )
 
 func TestRunInterrupt(t *testing.T) {
