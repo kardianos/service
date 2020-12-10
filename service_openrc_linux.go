423576c635b0fcc4783a2ba11278021a5fd2f9ec
Karen Almog <kalmog@mirantis.com>
2020-12-10 13:05:28 +0000

    fix a bugfix in command_args for openrc

--- service_openrc_linux.go
+++ service_openrc_linux.go
@@ -211,12 +211,13 @@ supervisor=supervise-daemon
 name="{{.DisplayName}}"
 description="{{.Description}}"
 command={{.Path|cmdEscape}}
-command_args={{range .Arguments}}{{.}}{{end}}
-
+{{- if .Arguments }}
+command_args="{{range .Arguments}}{{.}} {{end}}"
+{{- end }}
 name=$(basename $(readlink -f $command))
 supervise_daemon_args="--stdout /var/log/${name}.log --stderr /var/log/${name}.err"
 
-{{ if .Dependencies }}
+{{- if .Dependencies }}
 depend() {
 {{- range $i, $dep := .Dependencies}} 
 {{"\t"}}{{$dep}}{{end}}
