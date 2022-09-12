db90359f432c1be785930577631d9fb0503e262a
fumiya.aizawa <fumiya.aizawa@optim.co.jp>
2022-09-12 17:03:01 +0900

    add init.d env

--- service_sysv_linux.go
+++ service_sysv_linux.go
@@ -207,6 +207,11 @@ pid_file="/var/run/$name.pid"
 stdout_log="{{.LogDirectory}}/$name.log"
 stderr_log="{{.LogDirectory}}/$name.err"
 
+{{range $k, $v := .EnvVars -}}
+export {{$k}}={{$v}}
+{{$k}}={{$v}}
+{{end -}}
+
 [ -e /etc/sysconfig/$name ] && . /etc/sysconfig/$name
 
 get_pid() {
