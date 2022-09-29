kardianos/service

commit bc65850ac568bc54a66d0884274a54b4d0a19144

Allow setting environment variables for services (#312)
Setting environment variables for services running with systemd, OSx or
windows

service_windows.go
@@ -222,6 +222,27 @@ loop:
 	return false, 0
 }
 
+func (ws *windowsService) setEnvironmentVariablesInRegistry() error {
+	k, _, err := registry.CreateKey(
+		registry.LOCAL_MACHINE, `SYSTEM\CurrentControlSet\Services\`+ws.Name,
+		registry.QUERY_VALUE|registry.SET_VALUE|registry.CREATE_SUB_KEY)
+	if err != nil {
+		return fmt.Errorf("failed creating env var registry key, err = %v", err)
+	}
+	envStrings := make([]string, 0, len(ws.EnvVars))
+	for k, v := range ws.EnvVars {
+		envStrings = append(envStrings, k+"="+v)
+	}
+
+	if err := k.SetStringsValue("Environment", envStrings); err != nil {
+		return fmt.Errorf("failed setting env var registry key, err = %v", err)
+	}
+	if err := k.Close(); err != nil {
+		return fmt.Errorf("failed closing env var registry key, err = %v", err)
+	}
+	return nil
+}
+
 func (ws *windowsService) Install() error {
 	exepath, err := ws.execPath()
 	if err != nil {
@@ -233,6 +254,11 @@ func (ws *windowsService) Install() error {
 		return err
 	}
 	defer m.Disconnect()
+
+	if err := ws.setEnvironmentVariablesInRegistry(); err != nil {
+		return err
+	}
+
 	s, err := m.OpenService(ws.Name)
 	if err == nil {
 		s.Close()

service.go
@@ -134,6 +134,8 @@ type Config struct {
 
 	// System specific options.
 	Option KeyValue
+
+	EnvVars map[string]string
 }
 
 var (

service_darwin.go
@@ -285,6 +285,13 @@ var launchdConfig = `<?xml version='1.0' encoding='UTF-8'?>
 "http://www.apple.com/DTDs/PropertyList-1.0.dtd" >
 <plist version='1.0'>
   <dict>
+	<key>EnvironmentVariables</key>
+	<dict>
+	{{range $k, $v := .EnvVars -}}
+	<key>{{html $k}}</key>
+	<string>{{html $v}}</string>
+	{{end -}}
+	</dict>
     <key>Label</key>
     <string>{{html .Name}}</string>
     <key>ProgramArguments</key>

service_systemd_linux.go
@@ -318,6 +318,10 @@ StandardError=file:/var/log/{{.Name}}.err
 RestartSec=120
 EnvironmentFile=-/etc/sysconfig/{{.Name}}
 
+{{range $k, $v := .EnvVars -}}
+Environment={{$k}}={{$v}}
+{{end -}}
+
 [Install]
 WantedBy=multi-user.target
 `

commit 74ac9b45ed3932bf550d34e75570bf420ec5b8f4

Do not create windows services with an empty environment key
Doing so will create services that are broken and will fail
when started:
`Error 87: The parameter is incorrect`

This is a regression from https://github.com/kardianos/service/pull/312

service_windows.go
@@ -241,6 +241,10 @@ func lowPrivSvc(m *mgr.Mgr, name string) (*mgr.Service, error) {
 }
 
 func (ws *windowsService) setEnvironmentVariablesInRegistry() error {
+	if len(ws.EnvVars) == 0 {
+		return nil
+	}
+
 	k, _, err := registry.CreateKey(
 		registry.LOCAL_MACHINE, `SYSTEM\CurrentControlSet\Services\`+ws.Name,
 		registry.QUERY_VALUE|registry.SET_VALUE|registry.CREATE_SUB_KEY)